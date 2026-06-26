f325e3d7b30f9c71b2f79d006a0b05bc50661c65
sirlemz <55438528+sirlemz@users.noreply.github.com>
2026-06-26 21:40:13 +0800

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,12 @@ const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
 
+// Register schemas first before initializing connection string handlers
+require('./models/User');
+require('./models/Class');
+require('./models/Module');
+require('./models/Progress');
+
 require('./db'); // init DB
 
 const app = express();
